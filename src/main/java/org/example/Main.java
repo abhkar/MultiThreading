fccf70a8175aa929415f871912364c286b7a6733
abhishekmalakar <malakar.abh@gmail.com>
2022-12-22 21:20:29 +0530

    Changes Made

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,12 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n = 8; // Number of threads
+        for (int i = 0; i < n; i++) {
+            MultiThreadingDemo object
+                    = new MultiThreadingDemo();
+            object.start();
+        }
     }
 }
